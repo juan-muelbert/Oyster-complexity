06270dd6e4faab847f48ff5d5293183e7ec755ca
juan-muelbert <61525832+juan-muelbert@users.noreply.github.com>
2025-10-15 21:03:23 +1100

    Update paper.R

--- paper.R
+++ paper.R
@@ -14,6 +14,7 @@ library(pscl)
 
 complex_data <- read.csv("data_complexity_21oct2023.csv")
 metrics <- read.csv("tile_metrics.csv")
+metrics$logSurface_area <- log10(metrics$surface_area_cm2)
 data <- complex_data %>% left_join(metrics) %>% filter(treatment != "control") %>% na.omit()
 data$density <- data$oysters/data$surface_area_cm2
 data$site<-as.factor(data$site)
@@ -28,46 +29,50 @@ uncaged <- data %>% filter(treatment == "uncaged")
 # Main ====
 
 ### Complexity mediates predation on oyster recruits
-## Modelling the effect of rugosity on oyster counts and oyster densities, comparing treatment effects, using site as random effects
+## Surface area
+# Modelling the effect of surface area on oyster counts, comparing treatment effects, using site as random effects
 
-# Rugosity x Count
-rg.mod_count <- glmmTMB(oysters ~ poly(logRG, 2) * treatment + (1 | site),
-                        family = nbinom2(link = "log"),
+sa.count.mod <- glmmTMB(oysters ~ poly(logSurface_area, 2) * treatment + (1|site),
+                        family = nbinom2(link='log'),
                         data = data)
-summary(rg.mod_count)
-summary.rg.mod_count <- summary(rg.mod_count)
-hist(residuals(rg.mod_count))
+summary(sa.count.mod)
+summary.sa.count.mod <- summary(sa.count.mod)
+write.csv(summary.sa.count.mod$coefficients$cond, "SA_count_summary.csv")
+hist(residuals(sa.count.mod))
+ranef(sa.count.mod)
+r.squaredGLMM(sa.count.mod)
 
-# Fig 2a
+# Fig. 2a
 
-rugosity.new <- seq(from = min(data$logRG),
-                    to = max(data$logRG),
-                    length.out = 200)
-predic.rug <- tibble(logRG = rugosity.new) %>%
+surface_area.new <- seq(from = min(data$logSurface_area),
+                        to = max(data$logSurface_area),
+                        length.out = 200)
+
+predic.sa <- tibble(logSurface_area = surface_area.new) %>%
   expand_grid(tibble(treatment = c("caged", "uncaged"))) %>%
   expand_grid(tibble(site = c("brisbane_waters", "port_hacking", "hawkesbury")))
 
-rg_count.predict <- predict(object = rg.mod_count,
-                            newdata = predic.rug,
+sa.count.predict <- predict(object = sa.count.mod,
+                            newdata = predic.sa,
                             se.fit = TRUE,
                             type = "response") %>%
   as_tibble()
 
-plot_data_count <- bind_cols(predic.rug, rg_count.predict) %>%
+plot_data <- bind_cols(predic.sa, sa.count.predict) %>%
   mutate(confidence95 = se.fit*1.96)
 
-count_plot_data.summary <- plot_data_count %>% group_by(logRG, treatment) %>%
+plot_data.summary <- plot_data %>% group_by(logSurface_area, treatment) %>%
   summarise(fit = mean(fit), confidence95 = mean(confidence95))
 
-rg_count.plot <- ggplot(data = count_plot_data.summary, aes(x = logRG, y = fit, colour = treatment)) +
+sa.plot <- ggplot(data = plot_data.summary, aes(x = logSurface_area, y = fit, colour = treatment)) +
   geom_ribbon(aes(ymin = fit - confidence95,
-                  ymax = fit + confidence95), alpha = 0.3, linetype = 0) + 
-  geom_point(data = data, aes(x = logRG, y = oysters), alpha = 0.3, size = 2) +
+                  ymax = fit + confidence95), alpha = 0.3, linetype = 0) +
+  geom_point(data = data, aes(x = logSurface_area, y = oysters), alpha = 0.3, size = 2) +
   geom_line(linewidth = 1) + scale_colour_manual(values = c("#c54c00ff", "#5F5F5F")) +
   scale_fill_manual(values = alpha(c("#c54c00ff", "#5F5F5F"), 0.5)) + theme_classic() + ylab("Oyster count") +
-  xlab("log Rugosity") +  theme(legend.position = "none")
+  xlab("log10 surface area")
 
-rg_count.plot
+sa.plot
 
 ## Modelling the effect of Fractal dimension and Height Range on oyster oyster densities, comparing treatment effects, using site as random effects
 ## Fractal dimension
@@ -158,7 +163,6 @@ vif(fd.height_int)
 summary.fd_height <- summary(fd.height_int)
 summary.fd_height
 hist(residuals(fd.height_int))
-#write.csv(summary.fd_height$coefficients, "fd_height_full_mod.csv")
 
 # Plot model (heatmap) and overlay data from natural oyster reefs
 # summarizing oyster reef data
@@ -172,60 +176,35 @@ oyster_reefs <- reefs %>% group_by(patch) %>% # summarising reef points by patch
             seRG = sd(logRG)/sqrt(n()))
 
 # Fig3
+# Generate raw fig with visreg2d
 heatmap <- visreg2d(fd.height_int, xvar = "fd", yvar = "logHeight", plot.type ="gg", scale = "response",
                     data = uncaged)
+heatmap
+heatmap.df <- heatmap$data # extract heatmap data for plotting using ggplot 
+heatmap.df$z[heatmap.df$z < 0] <- NA
 
-heatmap.df <- heatmap$data
-max(heatmap.df$z) ## max predicted sqrt(density) = 0.1369969
+# Plot heatmap in ggplot
 
-heatmap.df$scaled_z <- rescale(heatmap.df$z, 
-                               to = c(0, 0.1369969), 
-                               from = range(heatmap.df$z,
-                                            na.rm = TRUE, finite = TRUE)) # scaling model from 0 to max predicted density(sqrt) to remove negative values 
+inc <- seq(0, max(sqrt(heatmap.df$z), na.rm=TRUE), length=20)^2 # create ribbons of 5% increments.
 
-# heatmap
-
-ggplot(data = heatmap.df, aes(x = x, y = y, fill = scaled_z)) +
+ggplot(data = heatmap.df, aes(x = x, y = y, fill = z)) +
   geom_raster() +
-  geom_contour(data = heatmap.df, aes(z=scaled_z), color="grey10", linewidth = 0.3, binwidth = 0.025) +
-  geom_point(data = oyster_reefs, aes (x = meanFD, y = meanlogHeight), size = 2, inherit.aes = FALSE) + 
+  geom_contour(data = heatmap.df, aes(z=z), color="black", linewidth = 0.3, breaks = inc) +
+  geom_point(data = oyster_reefs, aes (x = meanFD, y = meanlogHeight), size = 1.5, inherit.aes = FALSE) + 
   geom_errorbar(data = oyster_reefs, aes(x = meanFD, y = meanlogHeight,
                                          ymin = meanlogHeight - sdHeight,
-                                         ymax = meanlogHeight + sdHeight), width=0.01, inherit.aes = FALSE ) + 
+                                         ymax = meanlogHeight + sdHeight), width=0.01, inherit.aes = FALSE) + 
   geom_errorbar(data = oyster_reefs, aes(x = meanFD, y = meanlogHeight,
                                          xmin = meanFD - sdFD,
                                          xmax = meanFD + sdFD), width=0.01, inherit.aes = FALSE) +
-  scale_fill_viridis_c(option = "B") +
+  scale_fill_viridis_c(option = "B", na.value="white", begin = 0.05) +
   theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
                      panel.grid.minor = element_blank(), axis.line = element_line(colour = "grey50")) +
   xlab("Fractal dimension") + ylab("log Height range") + labs(fill = "Oysters per cm2 (sqrt)") +
-  scale_y_continuous(breaks = c(0.5, 0.6, 0.7, 0.8, 0.9, 1))
-
-
-### Relationship between Rugosity, fractal dimension and height range in the experimental design
-
-experimental_fd.height <- lm(logRG ~ fd * logHeight, data = metrics)
-summary(experimental_fd.height)
-
-# Fig 1c
-
-experimental_heatmap <- visreg2d(experimental_fd.height, xvar = "fd", yvar = "logHeight", plot.type ="gg", scale = "response",
-                                 data = metrics)
-
-experimental_heatmap.df <- experimental_heatmap$data
-
-ggplot(data = experimental_heatmap.df, aes(x = x, y = y, fill = z)) +
-  geom_raster() +
-  geom_point(data = metrics, aes(x = fd, y = logHeight), shape = 17, size = 4, colour = "black", inherit.aes = FALSE) +
-  geom_point(data = reefs, aes(x = fd, y = logHeight), shape = 4, size = 4, colour = "black", inherit.aes = FALSE) +
-  scale_fill_viridis_c(option = "A", begin = 0.3, end = 1) +
-  theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-                     panel.grid.minor = element_blank(), axis.line = element_line(colour = "grey50")) +
-  xlab("Fractal dimension") + ylab("log Height range") + labs(fill = "logRugosity") +
-  scale_y_continuous(breaks = c(0.5, 0.6, 0.7, 0.8, 0.9, 1))
+  scale_y_continuous(breaks = c(0.5, 0.6, 0.7, 0.8, 0.9, 1)) + 
+  theme(text = element_text(size = 7))
 
 # Suplementary materials ====
-
 ### Model testing for caging artifacts
 
 control_data <- read.csv("control.csv")
